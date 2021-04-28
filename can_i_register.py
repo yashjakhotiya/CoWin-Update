44403a05a0309982d403c4913ff1522e6d6a44c9
Yash Jakhotiya <mailsforyashj@gmail.com>
2021-04-28 17:50:41 +0530

    Stop hardcoding previous update time
    Source it from the last GET call

--- can_i_register.py
+++ can_i_register.py
@@ -12,18 +12,22 @@ def get_script_path(response):
     script_path = response[:idx][5:-1]
     return script_path
 
-def is_last_modified_updated():
+def is_last_modified_updated(prev_update_time):
     script_path = get_script_path(r.get(url).content)
     js_url = url + '/' + script_path
     last_modified_time = r.get(js_url).headers['last-modified']
-    if last_modified_time != 'Wed, 28 Apr 2021 10:16:31 GMT':
-        return True
-    return False
+    if last_modified_time != prev_update_time:
+        return True, last_modified_time
+    return False, last_modified_time
 
 idx = 0
+script_path = get_script_path(r.get(url).content)
+js_url = url + '/' + script_path
+prev_update_time = r.get(js_url).headers['last-modified']
 while(1):
     idx += 1
-    if (is_last_modified_updated()):
+    updated, prev_update_time = is_last_modified_updated(prev_update_time)
+    if (updated):
         print('Register now!\n')
         while(1):
             playsound('alarm.wav')
